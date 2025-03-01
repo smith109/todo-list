659c8c5eeef62e976783139ad0844a9e416ed21b
smith109 <devon.smith017@gmail.com>
2025-02-28 21:57:25 -0600

    Add a function to switch the active project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,18 @@ import createProject from "./modules/createProject";
 import dom from "./modules/dom";
 
 dom.projectForm.addEventListener('submit', submitProjectForm);
+dom.projectContainer.addEventListener('click', switchActiveProject);
+
+function switchActiveProject(e) {
+  const id = e.target.dataset.id;
+  const project = projectManager.findProject(id);
+  projectManager.setActiveProject(project);
+}
 
 function submitProjectForm(e) {
   const projectName = dom.projectForm.elements['project-name'].value;
   const project = createProject(projectName);
-  
+
   projectManager.addProject(project);
   dom.renderProjects(projectManager.getProjects());
 
