85c0d32195ea620dc248ad374cd0bdeb528fc8e3
smith109 <devon.smith017@gmail.com>
2025-03-04 22:20:48 -0600

    Add a factory function to create project objects

---
+++ src/modules/createProject.js
@@ -0,0 +1,19 @@
+const createProject = (name = 'Inbox') => {
+  const todos = [];
+
+  const getName = () => name;
+  const getTodos = () => [...todos];
+
+  function addTodo(todo) {
+    todos.push(todo);
+  }
+
+  function removeTodo(todo) {
+    const index = todos.indexOf(todo);
+    todos.splice(index, 1);
+  }
+
+  return { getName, getTodos, addTodo, removeTodo };
+}
+
+export default createProject;
