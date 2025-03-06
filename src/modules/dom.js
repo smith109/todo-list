af40c09c579da9b5f6713e699920f00ad21e5b37
smith109 <devon.smith017@gmail.com>
2025-03-06 07:52:31 -0600

    Add a function to render the project list

---
+++ src/modules/dom.js
@@ -0,0 +1,18 @@
+const projectContainer = document.querySelector('.projects');
+
+function createProjectElement(project) {
+  const projectElement = document.createElement('li');
+  projectElement.textContent = project.getName();
+  return projectElement;
+}
+
+function renderProjects(projectArr) {
+  projectArr.forEach((project) => {
+    const projectElement = createProjectElement(project);
+    projectContainer.append(projectElement);
+  });
+}
+
+export default {
+  renderProjects
+}
