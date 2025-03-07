9e4654c1bf1d9166f79e34b35b42011366a265f2
smith109 <devon.smith017@gmail.com>
2025-03-06 21:41:22 -0600

    Add a function to render the todo list

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,4 +1,5 @@
 const projectContainer = document.querySelector('.projects');
+const todoContainer = document.querySelector('.todos');
 
 function createProjectElement(project) {
   const projectElement = document.createElement('li');
@@ -13,6 +14,48 @@ function renderProjects(projectArr) {
   });
 }
 
+function createTodoElement(todo) {
+  const todoElement = document.createElement('div');
+  const todoHeader = document.createElement('div');
+  const checkboxLabel = document.createElement('label');
+  const checkboxInput = document.createElement('input');
+  const todoTitle = document.createElement('h3');
+  const todoPriority = document.createElement('p');
+  const todoDueDate = document.createElement('p');
+  const todoDetails = document.createElement('div');
+  const todoDescription = document.createElement('p');
+  const btnContainer = document.createElement('div');
+  const editBtn = document.createElement('button');
+  const deleteBtn = document.createElement('button');
+
+  todoElement.classList.add('todo-card');
+  todoHeader.classList.add('header');
+  todoDetails.classList.add('todo-details');
+  checkboxInput.type = 'checkbox';
+
+  todoTitle.textContent = todo.getTitle();
+  todoPriority.textContent = todo.getPriority();
+  todoDueDate.textContent = todo.getDueDate();
+  todoDescription.textContent = todo.getDescription();
+  editBtn.textContent = 'Edit';
+  deleteBtn.textContent = 'Delete';
+
+  checkboxLabel.append(checkboxInput);
+  btnContainer.append(editBtn, deleteBtn);
+  todoHeader.append(checkboxLabel, todoTitle, todoPriority, todoDueDate);
+  todoDetails.append(todoDescription, btnContainer);
+  todoElement.append(todoHeader, todoDetails);
+  return todoElement;
+}
+
+function renderTodos(todoArr) {
+  todoArr.forEach((todo) => {
+    const todoElement = createTodoElement(todo);
+    todoContainer.append(todoElement);
+  });
+}
+
 export default {
-  renderProjects
+  renderProjects,
+  renderTodos
 }
