39a7a496de58cb1c03455696cf961b24f6df90c2
smith109 <devon.smith017@gmail.com>
2025-03-08 19:12:47 -0600

    Add functions to open and close the todo modal

--- src/index.js
+++ src/index.js
@@ -3,14 +3,19 @@ import createProject from "./modules/createProject";
 import dom from "./modules/dom";
 
 const addProjectBtn = document.querySelector('.add-project-btn');
-const projectForm = document.querySelector('.project-form');
 const projectCancelBtn = document.querySelector('.project-form .cancel-btn');
+const projectForm = document.querySelector('.project-form');
 const projectContainer = document.querySelector('.projects');
+const addTodoBtn = document.querySelector('.add-todo-btn');
+const todoCancelBtn = document.querySelector('.todo-form .cancel-btn');
+const todoForm = document.querySelector('.todo-form');
 
 addProjectBtn.addEventListener('click', dom.openProjectModal);
-projectForm.addEventListener('submit', submitProjectForm);
 projectCancelBtn.addEventListener('click', closeProjectForm);
+projectForm.addEventListener('submit', submitProjectForm);
 projectContainer.addEventListener('click', changeActiveProject);
+addTodoBtn.addEventListener('click', dom.openTodoModal);
+todoCancelBtn.addEventListener('click', closeTodoForm);
 
 function submitProjectForm(e) {
   e.preventDefault();
@@ -25,15 +30,20 @@ function submitProjectForm(e) {
   dom.renderProjects(projectManager.getProjects());
 }
 
+function changeActiveProject(e) {
+  if (e.target.tagName !== 'LI') return;
+  const id = e.target.dataset.id;
+
+  projectManager.setActiveProject(projectManager.findProject(id));
+  dom.updateProjectName(projectManager.getActiveProject());
+}
+
 function closeProjectForm() {
   projectForm.reset();
   dom.closeProjectModal();
 }
 
-function changeActiveProject(e) {
-  if (e.target.tagName !== 'LI') return;
-  const id = e.target.dataset.id;
-  
-  projectManager.setActiveProject(projectManager.findProject(id));
-  dom.updateProjectName(projectManager.getActiveProject());
+function closeTodoForm() {
+  todoForm.reset();
+  dom.closeTodoModal();
 }
