e0f71825ecffa6f81e88c7033095961481e88376
smith109 <devon.smith017@gmail.com>
2025-03-08 21:42:05 -0600

    Add a function to submit the todo form

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import projectManager from "./modules/projectManager";
 import createProject from "./modules/createProject";
+import createTodo from "./modules/createTodo";
 import dom from "./modules/dom";
 
 const addProjectBtn = document.querySelector('.add-project-btn');
@@ -11,11 +12,12 @@ const todoCancelBtn = document.querySelector('.todo-form .cancel-btn');
 const todoForm = document.querySelector('.todo-form');
 
 addProjectBtn.addEventListener('click', dom.openProjectModal);
-projectCancelBtn.addEventListener('click', closeProjectForm);
+projectCancelBtn.addEventListener('click', closeProjectModal);
 projectForm.addEventListener('submit', submitProjectForm);
 projectContainer.addEventListener('click', changeActiveProject);
-addTodoBtn.addEventListener('click', dom.openTodoModal);
-todoCancelBtn.addEventListener('click', closeTodoForm);
+addTodoBtn.addEventListener('click', openTodoModal);
+todoCancelBtn.addEventListener('click', closeTodoModal);
+todoForm.addEventListener('submit', submitTodoForm);
 
 function submitProjectForm(e) {
   e.preventDefault();
@@ -30,20 +32,46 @@ function submitProjectForm(e) {
   dom.renderProjects(projectManager.getProjects());
 }
 
+function submitTodoForm(e) {
+  e.preventDefault();
+  const formData = todoForm.elements;
+
+  const todo = createTodo(
+    formData['todo-title'].value,
+    formData['todo-description'].value,
+    formData['todo-due-date'].value,
+    formData['todo-priority'].value
+  );
+
+  todoForm.reset();
+  dom.closeTodoModal();
+  projectManager.getActiveProject().addTodo(todo);
+  dom.renderTodos(projectManager.getActiveProject().getTodos());
+}
+
 function changeActiveProject(e) {
   if (e.target.tagName !== 'LI') return;
   const id = e.target.dataset.id;
 
   projectManager.setActiveProject(projectManager.findProject(id));
   dom.updateProjectName(projectManager.getActiveProject());
+  dom.renderTodos(projectManager.getActiveProject().getTodos());
 }
 
-function closeProjectForm() {
+function openTodoModal() {
+  if (!projectManager.getActiveProject()) {
+    alert('Please select a project');
+  } else {
+    dom.openTodoModal();
+  }
+}
+
+function closeProjectModal() {
   projectForm.reset();
   dom.closeProjectModal();
 }
 
-function closeTodoForm() {
+function closeTodoModal() {
   todoForm.reset();
   dom.closeTodoModal();
 }
