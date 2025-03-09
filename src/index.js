0e7efe6b96dfa434094c383db0b57c13ced04627
smith109 <devon.smith017@gmail.com>
2025-03-08 18:00:04 -0600

    Add a function to change the active project

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import dom from "./modules/dom";
 const addProjectBtn = document.querySelector('.add-project-btn');
 const projectForm = document.querySelector('.project-form');
 const projectCancelBtn = document.querySelector('.project-form .cancel-btn');
+const projectContainer = document.querySelector('.projects');
 
 addProjectBtn.addEventListener('click', dom.openProjectModal);
 projectForm.addEventListener('submit', submitProjectForm);
 projectCancelBtn.addEventListener('click', closeProjectForm);
+projectContainer.addEventListener('click', changeActiveProject);
 
 function submitProjectForm(e) {
   e.preventDefault();
@@ -26,4 +28,11 @@ function submitProjectForm(e) {
 function closeProjectForm() {
   projectForm.reset();
   dom.closeProjectModal();
+}
+
+function changeActiveProject(e) {
+  if (e.target.tagName !== 'LI') return;
+  const id = e.target.dataset.id;
+  
+  projectManager.setActiveProject(projectManager.findProject(id));
 }
