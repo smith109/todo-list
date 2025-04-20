40355b1db30c84518ac11b581afd135c5b709e1a
smith109 <devon.smith017@gmail.com>
2025-04-19 21:20:25 -0500

    Add functions to manage the project list

---
+++ src/modules/projectManager.js
@@ -0,0 +1,35 @@
+const projects = [];
+let activeProject = '';
+
+const getProjects = () => [...projects];
+const getActiveProject = () => activeProject;
+
+function setActiveProject(project) {
+  activeProject = project;
+}
+
+function addProject(project) {
+  projects.push(project);
+}
+
+function removeProject(project) {
+  const index = projects.indexOf(project);
+  projects.splice(index, 1);
+
+  if (project === activeProject) {
+    activeProject = projects.length > 0 ? projects[0] : '';
+  }
+}
+
+function findProject(id) {
+  return projects.find((project) => project.getId() === id);
+}
+
+export default {
+  getProjects,
+  getActiveProject,
+  setActiveProject,
+  addProject,
+  removeProject,
+  findProject
+}
