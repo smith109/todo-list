fb85d3831665283c4b318ce1b2fcf78db1788b12
smith109 <devon.smith017@gmail.com>
2025-07-17 23:18:46 -0500

    Add a function to create the todo header element

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -26,6 +26,32 @@ const renderProjects = (projectArr) => {
   });
 }
 
+const createTodoHeader = (todo) => {
+  const header = document.createElement('div');
+  const checkboxLabel = document.createElement('label');
+  const checkboxInput = document.createElement('input');
+  const todoSummaryDiv = document.createElement('div');
+  const titleH3 = document.createElement('h3');
+  const prioritySpan = document.createElement('span');
+  const dueDateSpan = document.createElement('span');
+
+  header.classList.add('header');
+  todoSummaryDiv.classList.add('todo-summary');
+  prioritySpan.classList.add(`${todo.priority}-priority-text`);
+
+  checkboxInput.setAttribute('type', 'checkbox');
+  checkboxInput.setAttribute('name', 'todo-done');
+
+  titleH3.textContent = todo.title;
+  prioritySpan.textContent = `${todo.priority} Priority`;
+  dueDateSpan.textContent = todo.dueDate ? `Due: ${todo.dueDate}` : '';
+
+  checkboxLabel.append(checkboxInput);
+  todoSummaryDiv.append(titleH3, prioritySpan);
+  header.append(checkboxLabel, todoSummaryDiv, dueDateSpan);
+  return header;
+}
+
 export default {
   renderActiveProjectName,
   renderProjects,
