a0afb8042d9a6a09b03dea10d886ed543ba5e34b
smith109 <devon.smith017@gmail.com>
2025-07-17 22:18:54 -0500

    Add a function to render the project list

--- src/modules/dom.js
+++ src/modules/dom.js
@@ -1,9 +1,32 @@
 const activeProjectName = document.querySelector('.active-project-name');
+const projectContainer = document.querySelector('.project-container');
 
 const renderActiveProjectName = (activeProject) => {
   activeProjectName.textContent = activeProject.name;
 }
 
+const createProjectElement = (project) => {
+  const projectElement = document.createElement('li');
+  const projectNameSpan = document.createElement('span');
+  const deleteBtn = document.createElement('button');
+
+  projectElement.dataset.id = project.getId();
+  projectNameSpan.textContent = project.name;
+
+  projectElement.append(projectNameSpan, deleteBtn);
+  return projectElement;
+}
+
+const renderProjects = (projectArr) => {
+  projectContainer.replaceChildren();
+
+  projectArr.forEach((project) => {
+    const projectElement = createProjectElement(project);
+    projectContainer.append(projectElement);
+  });
+}
+
 export default {
   renderActiveProjectName,
+  renderProjects,
 }
