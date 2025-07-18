5726b2ade7b4127fecdfd2ecc2b790e8d1eba218
smith109 <devon.smith017@gmail.com>
2025-07-17 20:37:27 -0500

    Add a class to create project objects

---
+++ src/modules/Project.js
@@ -0,0 +1,33 @@
+export default class Project {
+  #id = crypto.randomUUID();
+  #todos = [];
+
+  constructor(name = 'Inbox') {
+    this.name = name;
+  }
+
+  getId() {
+    return this.#id;
+  }
+
+  getTodos() {
+    return [...this.#todos];
+  }
+
+  addTodo(todo) {
+    this.#todos.push(todo);
+  }
+
+  removeTodo(id) {
+    this.#todos = this.#todos.filter((todo) => todo.getId() !== id);
+  }
+
+  findTodoById(id) {
+    return this.#todos.find((todo) => todo.getId() === id);
+  }
+
+  updateTodo(id, newDetails = {}) {
+    const todoToEdit = this.findTodoById(id);
+    Object.assign(todoToEdit, newDetails);
+  }
+}
