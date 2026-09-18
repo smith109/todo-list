e35e9674a0098f0f0ad35624c32fa13909af37f9
smith109 <devon.smith017@gmail.com>
2026-09-18 15:28:31 -0500

    Add logic to create new project objects

---
+++ src/modules/Project.js
@@ -0,0 +1,28 @@
+export class Project { 
+  #id = crypto.randomUUID();
+  #todos = [];
+
+  constructor(name = 'Inbox') {
+    this.name = name;
+  }
+
+  get id() {
+    return this.#id;
+  }
+
+  get todos() {
+    return [...this.#todos];
+  }
+
+  add(todo) {
+    this.#todos.push(todo);
+  }
+
+  remove(id) {
+    this.#todos = this.#todos.filter((todo) => todo.id !== id);
+  }
+
+  find(id) {
+    return this.#todos.find((todo) => todo.id === id);
+  }
+}
