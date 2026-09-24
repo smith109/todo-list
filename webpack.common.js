8cf474baca3ee5ff788682c7cc5480e6709e574d
smith109 <devon.smith017@gmail.com>
2026-09-24 09:10:39 -0500

    Add module rule to read html-loader plugin

--- webpack.common.js
+++ webpack.common.js
@@ -19,6 +19,10 @@ export default {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      }
     ],
   },
 };
